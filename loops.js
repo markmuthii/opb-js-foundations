044f4ac1c5f9db7359c3a12e503bba37980681cf
Mark Muthii <mark.muthii@strathmore.edu>
2025-06-28 10:29:47 +0300

    Loops

---
+++ loops.js
@@ -0,0 +1,81 @@
+// Declaration -
+// let num;
+
+// // Assignment
+// num = 7;
+
+// // Initialization
+// let num2 = 77;
+
+// Block of code
+// Executed repeatedly
+// For as long as a certain condition is true
+
+// 1. While Loop
+// 2. For Loop
+// 3. Do...While Loop
+// 4. For...In Loop
+// 5. For...Of Loop
+// 6.
+
+// 1. FOR LOOP
+
+// SYNTAX:
+// for (initialization; condition; update) {
+//   // Block of code to be executed repeatedly
+// }
+
+// console.log("Before the loop");
+
+// // Print numbers from 1 to 10
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// console.log("Done looping");
+
+// 2. WHILE
+// Syntax:
+// while (condition) {
+//   // Code to be executed
+// }
+
+console.log("Before the loop");
+
+let i = 1;
+
+while (i <= 3) {
+  console.log(i);
+
+  i++;
+}
+
+console.log(i);
+
+console.log("Done looping");
+
+// 3. DO...WHILE
+// Syntax:
+// do {
+//   // Code to be executed
+// } while (condition);
+
+// console.log("Before the loop");
+
+// let i = 4;
+
+// do {
+//   console.log(i);
+
+//   i++;
+// } while (i >= 3);
+
+// console.log("Done looping");
+
+// let num = 7;
+
+// console.log(num);
+
+// num = 77;
+
+// console.log(num);
